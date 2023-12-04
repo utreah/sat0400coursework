fd44901071198f7727e6a65726f94aafbc7098f5
utreah <ersoyugur1528@gmail.com>
2023-12-04 20:08:16 +0000

    Update coursework_v2.py

--- coursework_v2.py
+++ coursework_v2.py
@@ -2,16 +2,16 @@ import time
 import os 
 import random
 # TO DO LIST: 
-# [WIP?] MAKE A TEST TABLE AND SEND IT WITH COURSEWORK -> NOT STARTED YET
+# [WIP?] MAKE A TEST TABLE AND SEND IT WITH COURSEWORK -> WORKING ON IT
 # [DONE] converting between data types.
 # [DONE] Customer Food/Drink append method needs to be compeletly changed.
 # [DONE] Coupon apply method completely changed and old version has been commented (for future references)
 # [DONE] ADD while loop to customer_coupon_answer so if the customer types something different than y/n, they'll have another chance (maybe we can add a limit like max 3 attempts)
 # [DONE] FINISH VALIDITYCHECKS FOR NAME -> NOT STARTED AT ALL <- 4 days later "DONE"
 # [DONE] MERGE CUSTOMERPAYMENTPAGE AND COUPON FUNCTION. MAKE THEM WORK TOGETHER -> ALMOST DONE, NEED TO CODE SMT TO CHECK MONTH AND DAY CHECK FOR CARD PAYMENTS <- "DONE"
-# [BUG/SOLVED] CHANGE WHILE LOOP (AT LEAST ONE OF THEM MUST HAVE A CONDITION) - "SOLVED" -> now, the first while loop has a condition to exit.
+# [DONE] CHANGE WHILE LOOP (AT LEAST ONE OF THEM MUST HAVE A CONDITION) - "SOLVED" -> now, the first while loop has a condition to exit.
 # [BUG/SOLVED] CHECK ADJUST PRICE AND ITEM REMOVE FUNCTIONS. SMT WRONG WITH ADJUST PRICE. DOESN'T DELETE THE PRICE PROPERLY(%50 OF THE CASES) -> "SOLVED", total_price has been moved inside the loop. It wasn't being updated after adjustPrice function
-# [BUG/SOLVED] It applies discount, changing total_price but the first line of the while loop rewrites the original price back. -> total price adjuster has been moved outside the while loop, now prices are being updated every time a customer adds/removes an item.
+# [BUG/SOLVED] It applies discount, changing total_price but first line of the while loop rewrites the original price back. -> total price adjuster has been moved outside the while loop, now prices are being updated every time a customer adds/removes an item.
 # [BUG/SOLVED] Another problem related to remove/add item function. "SOLVED" -> Removes the first item that the function encounters, then updates the price | SHOULD BE FIXED :(
 # [BUG/SOLVED] If mm/dd is wrong (less than 1 or above 12) it still prints successful. -> "SOLVED" whole thing has been rewritten from scratch. Works flawlessly, killed test cases.
 # [BUG/SOLVED] After finishing the payment, customer_menu_choice still takes input. Find a way to fix it -> "SOLVED" just added "break" to option 4 :D
@@ -30,7 +30,14 @@ def is_valid_name(customer_name):
         if char in name_check:
             return False  # Name contains a symbol or number
     return True  # Name is valid
-        
+
+def is_valid_age(customer_age):
+    # age_int_check = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','@','#','$','%','^','&','*','_','+','-']
+    if customer_age.isdigit():
+        return True
+    else:
+        return False
+
 def customer_printMenu():
 
 ##    beer_random_id1 = random.randint(0,2) # assign a new id everytime it prints the menu between the range to avoid people <18 to order alcoholic beverages. 
@@ -64,7 +71,7 @@ def customer_appendChoices(customer_item_choices, customer_age):
                 selected_items.append(itemNameToAppend) # append food name
                 selected_items_total_price.append(price) # append Its price
                 item_found = True
-    if not item_found:
+    if not item_found and age_restricted == False:
         print(f"Item ''{customer_item_choices}'' cannot be found. Please check your spelling or make sure that item is on the menu!")
 
 def print_customer_basket(selected_items,selected_items_total_price):
@@ -77,6 +84,8 @@ def print_customer_basket(selected_items,selected_items_total_price):
             print(f"{orderCounter} x{foodCounter}: {price*foodCounter}£") # prints everything we found
             seenFood.add(orderCounter) # adds it to seenFood list so we wont count it more than one
             customer_basket_totalPrice += price
+    if customer_basket_totalPrice <= 0:
+        print("Customer basket is empty")
     
 def customer_basket_total_price(selected_items_total_price):
     customer_basket_totalPrice = 0
@@ -109,12 +118,15 @@ def customer_adjustPrice(removeItem_from_Basket, selected_items_total_price):
         print("Error: Price not found for the removed item.") # if not print error or pass
 
 def customer_applyCoupon(customerCoupon,total_price_discounted_by_coupon):
-    couponList = ["MDX10","MDX25","MDX50"]
-    couponDiscountPercantage = [10/100,25/100,50/100]
-    if customerCoupon in couponList:
-        indexOf_coupon_to_apply = couponList.index(customerCoupon)
-        print(f"Coupon {customerCoupon} has been validated and applied on Basket Amount.") 
-        return total_price_discounted_by_coupon-(total_price_discounted_by_coupon*couponDiscountPercantage[indexOf_coupon_to_apply])
+    coupon_attempt_counter = 0
+    while coupon_attempt_counter < 3:
+        customerCoupon = input(f"Attempt ({coupon_attempt_counter+1}): Can I have the coupon please? :")
+        couponList = ["MDX10","MDX25","MDX50"]
+        couponDiscountPercantage = [10/100,25/100,50/100]
+        if customerCoupon in couponList:
+            indexOf_coupon_to_apply = couponList.index(customerCoupon)
+            print(f"Coupon ({customerCoupon}) has been validated and applied on Basket Amount.") 
+            return total_price_discounted_by_coupon-(total_price_discounted_by_coupon*couponDiscountPercantage[indexOf_coupon_to_apply])
     #for validCoupon in couponList: # loops through coupon list
     #if customerCoupon == couponList[0]:
     #    print(f"Coupon '{couponList[0]}' has been validated and applied to total price!")
@@ -125,12 +137,16 @@ def customer_applyCoupon(customerCoupon,total_price_discounted_by_coupon):
     #elif customerCoupon == couponList[2]:
     #    print(f"Coupon '{couponList[2]}' has been validated and applied to total price!")
     #    return total_price_discounted_by_coupon-(total_price_discounted_by_coupon*50/100)
-    else:
-        print("Coupon could not be applied! Original price has been returned")
-        return total_price_discounted_by_coupon
+        else:
+            coupon_attempt_counter += 1
+            print("Attempt Left :",3-coupon_attempt_counter)
+            if coupon_attempt_counter >= 3:
+                print("Coupon Could not be verified. Original price has been returned!.\nYou are being redirected to payment page")
+                return total_price_discounted_by_coupon
                 
-def customer_payment_page(total_price): # NOTE to REVIEWER: JUST USE SOME RANDOM NUMBERS IT WILL ACCEPT ANYWAY ( I'll add some checks for date tho ) <- "DONE"
-    print(f"Amount that needs to be paid : {total_price}£")
+def customer_payment_page(total_price): # NOTE to MYSELF: JUST USE SOME RANDOM NUMBERS IT WILL ACCEPT ANYWAY ( I'll add some checks for date tho ) <- "DONE"
+    current_computer_time = time.strftime("%H:%M:%S")
+    print(f"Amount that needs to be paid : {total_price:.2f}£")
     customer_coupon_answer_attempts = 0
     while customer_coupon_answer_attempts < 3:
         if customer_coupon_answer_attempts != 0:
@@ -182,7 +198,7 @@ def customer_payment_page(total_price): # NOTE to REVIEWER: JUST USE SOME RANDOM
         
         # Validate month and day
                 if 1 <= customer_card_valid_date_mm <= 12 and 1 <= customer_card_valid_date_dd <= 31:
-                    print("Payment has been processed! Have a nice day!")
+                    print("Payment has been processed at " + current_computer_time + ",Have a nice day!")
                     break
                 else:
                     print("Invalid month or day in the date. Please try again.")
@@ -194,7 +210,7 @@ def customer_payment_page(total_price): # NOTE to REVIEWER: JUST USE SOME RANDOM
     # Add the case for cash payment (too lazy to do anything so this WILL NEED TO do the work)
     elif "2" in customer_payment_way:
         print("Payment by cash selected.")
-        print("Customer has been paid. Have a nice day!")
+        print(f"This table has been paid at {current_computer_time}, have a nice day!")
     else:
         print("Invalid payment choice.")
         
@@ -202,16 +218,24 @@ def customer_payment_page(total_price): # NOTE to REVIEWER: JUST USE SOME RANDOM
     
 total_price = 0
 customer_name = str(input("Name : "))
-is_valid = False
-while is_valid != True:
+is_customer_name_valid = False
+while is_customer_name_valid != True:
     if is_valid_name(customer_name):
         print("Hello "+customer_name,"Welcome to our Cafe!")
         break
     else:
         customer_name=input("Invalid name. Name should not contain symbols or numbers.\nPlease enter your name again. :")
+is_customer_age_valid = False
+customer_age_str = input("I need to know your age to show our menu. How old are you? : ")
+
+while is_customer_age_valid != True:
+    if is_valid_age(customer_age_str):
+        customer_age = int(customer_age_str)
+        print("Okay,",customer_name,"you are ",customer_age," years old.")
+        break
+    else:
+        customer_age_str = input("I need to know your age to show our menu. How old are you? : ")
     
-customer_age = int(input("I need to know your age to show our menu. How old are you? : "))
-print("Okay ",customer_name,"you are ",customer_age," years old.")
 print("Menu will be printed in 5 seconds and this screen will be cleared!")
 time.sleep(5)
 os.system("clear||cls")
@@ -219,28 +243,34 @@ customer_printMenu()
 
 while_dummy_variable = "x" # dont mind it just some dummy variable that I created for while loop
 QUIT_KEY = "q" # handsome quit_key #define QUIT_KEY "q" ??
-
+GET_CURRENT_TIME = time.strftime("%H:%M:%S")
 while while_dummy_variable != QUIT_KEY:
         customer_Take_Order = input("What would you like to order? (Type 'q' to quit): ")
         while_dummy_variable = customer_Take_Order
         #if customer_Take_Order == 'q':
         #    break
         customer_appendChoices(customer_Take_Order, customer_age)
+os.system("clear||cls")
 total_price = customer_basket_total_price(selected_items_total_price)
 while True:
     customer_menu_choice = int(input("Choose one of the following\n1-Print Items in the basket and total price \n2-Remove Item\n3-Add Item\n4-Payment/Discount Page\nChoice: "))
     if customer_menu_choice == 1:
         os.system("clear||cls")
         print_customer_basket(selected_items,selected_items_total_price)
-        print(f"Total Price: {total_price}£")
+        print(f"Total Price: {total_price:.2f}£")
+        print(GET_CURRENT_TIME)
+        print("------------------------------")
         total_price = customer_basket_total_price(selected_items_total_price)
     elif customer_menu_choice == 2:
+        os.system("clear||cls")
         print_customer_basket(selected_items,selected_items_total_price)
         itemtoRemove = str(input("Which Item would you like to remove? : "))
         customer_removeItem(itemtoRemove,selected_items,selected_items_total_price)
         total_price = customer_basket_total_price(selected_items_total_price)
        # customer_adjustPrice(itemtoRemove, selected_items_total_price) <- doesn't work(need to rewrite whole adjustPrice function to make this work)UPDATE:problem solved without rewriting the whole function. Will keep it for test case(s)
     elif customer_menu_choice == 3:
+        os.system("clear||cls")
+        customer_printMenu()
         while True:
             customer_Take_Order = input("What would you like to order? (Type 'q' to quit): ")
             if customer_Take_Order == 'q':
